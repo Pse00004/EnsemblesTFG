e1527a934acb25f3609c9e51d15378531192d354
Patri <pse@pse>
2019-03-30 21:52:01 +0100

    Limpiar clase Dataset

--- src/main/scala/prueba/Dataset.scala
+++ src/main/scala/prueba/Dataset.scala
@@ -5,151 +5,166 @@ import org.apache.spark.rdd.RDD
 
 class DataSet extends Serializable {
 
-     //type Instance = (Vector[Double], Vector[Double])
-     var task: Char = 'c'
-     var nInput: Int = 0
-     var nOutput: Int = 0
-     var nAttributes: Int = 0
-     var nInstances: Int = 0
-     var outputValues: Array[String] = null
-     var attributes: Array[Attribute] = null
+    var nInput: Int = 0
+    var nOutput: Int = 0
+    var nAttributes: Int = 0
+    var nInstances: Int = 0
+    var outputValues: Array[String] = null
+    var attributes: Array[Attribute] = null
 
-     var instances: RDD[(Vector[Double], Vector[Double])] = null
-     //val sc = new SparkContext((new SparkConf).setAppName("Attribute").setMaster("local[8]"))
+    var instances: RDD[(Vector[Double], Vector[Double])] = null
+    //val sc = new SparkContext((new SparkConf).setAppName("Attribute").setMaster("local[8]"))
 
-     def getTask = this.task
-     def getnInput = this.nInput
-     def getnOutput = this.nOutput
-     def getnAttributes = this.nAttributes
-     def getOutputValues = this.outputValues
-     def getAttributes = this.attributes
-     def getInstances = this.instances
-     def getnInstances = this.nInstances
+    def getnInput = this.nInput
 
-     def loadDataSet(file: String, sc: SparkContext) = {
+    def getnOutput = this.nOutput
 
-          val lines = sc.textFile(file).cache()
+    def getnAttributes = this.nAttributes
 
-          val head = lines.filter { line => line.startsWith("@") }
-          val linesHead = head.filter { line => line.startsWith("@attribute") }
-          nInput = head.filter(line => line.startsWith("@inputs")).first().split("( *),( *)|( +)").length - 1
-          outputValues = head.filter(line => line.startsWith("@outputs")).first().split("( *),( *)|( +)")
+    def getOutputValues = this.outputValues
 
-          nOutput = outputValues.length - 1
-          nAttributes = nInput + nOutput
+    def getAttributes = this.attributes
 
-          val linesInstances = lines.filter(line => !line.contains("@") && !line.trim().isEmpty)
+    def getInstances = this.instances
 
-          loadAttributes(linesHead)
-          loadInstances(linesInstances)
+    def getnInstances = this.nInstances
 
-          //summaryNominalAttributes()
-          println("Loading, inputs: " + nInput + " outputs: " + nOutput + " total attributes " + nAttributes)
-          //printInstances()
-     }
+    def loadDataSet(file: String, sc: SparkContext) = {
 
-     def loadAttributes(lines: RDD[String]) = {
+        val lines = sc.textFile(file).cache()
 
-          var io = ' '
-          val wordsLines = lines.map(line => line.split("( *)(\\{)( *)|( *)(\\})( *)|( *)(\\[)( *)|( *)(\\])( *)|( *)(,)( *)| "))
-          println("Atributos")
-          attributes = wordsLines.collect().map {
-               words =>
-                    words.foreach(println)
-                    if (outputValues.indexOf(words(1)) != -1) io = 'o' else io = 'i'
-                    if (words(2) == "real" || words(2) == "integer") Attribute(words(1), 'r', io, words(3).toDouble, words(4).toDouble, Array("Void")) //call the constructor real attribute
-                    else Attribute(words(1), 'n', io, 0, words.length - 3, words.slice(2, words.length)) //call the constructor nominal attribute
-          }
+        val head = lines.filter { line => line.startsWith("@") }
+        val linesHead = head.filter { line => line.startsWith("@attribute") }
+        nInput = head.filter(line => line.startsWith("@inputs")).first().split("( *),( *)|( +)").length - 1
+        outputValues = head.filter(line => line.startsWith("@outputs")).first().split("( *),( *)|( +)")
 
-          // if classification we goint to consider the number of output attributes equal to number of different values of this attribute
-          if (task == 'c') {
-               outputValues = attributes.last.valNominal
-               attributes.last.name = outputValues(0)
-               attributes.last.rangeMax = 1
-               attributes.last.rangeMin = 0
-               attributes.last.valNominal = Array("0", "1")
-               for (i <- 1 to outputValues.length - 1) {
-                    attributes = attributes :+ Attribute(outputValues(i), 'n', 'o', 0.0, 1.0, Array("0", "1"))
-               }
+        nOutput = outputValues.length - 1
+        nAttributes = nInput + nOutput
 
-               // update the number of outputs
-               nOutput = outputValues.length
-               nAttributes = nInput + nOutput
-          }
-          attributes.foreach(atb => if (atb.kind == 'n') atb.numInstValue = Array.fill(atb.valNominal.length)(0))
-     }
+        val linesInstances = lines.filter(line => !line.contains("@") && !line.trim().isEmpty)
 
-     def loadInstances(lines: RDD[String]) = {
-          // No possible to use a class variable inside a map
-          // No possible to reference a RDD inside a map of other RDD
-          // unless used extend serializable
+        loadAttributes(linesHead)
+        loadInstances(linesInstances)
 
-          println("Numero de salidas: " + nOutput)
-          val nInp = nInput;
-          val tsk = task
-          val outV = outputValues
+        println("Inputs: " + nInput + " Outputs: " + nOutput + " Total attributes: " + nAttributes)
+    }
 
-          val auxInstances = lines.map { line => line.split("( *)(,)( *)") }
-          val auxRDD = auxInstances.map { line =>
-               var i = -1;
-               val auxVectorInput = new Array[Double](nInp);
-               val auxVectorOutput = new Array[Double](nOutput);
-               line.foreach { x =>
-                    i = i + 1;
-                    if (attributes(i).IO == 'i') {
-                         if (attributes(i).kind == 'r') auxVectorInput(i) = x.toDouble
-                         else {
-                              auxVectorInput(i) = attributes(i).valNominal.indexOf(x)
-                              if (auxVectorInput(i) == -1) println("*****ERROR " + x)
-                         }
-                    } else {
-                         if (tsk == 'm')
-                              auxVectorOutput(i - nInp) = x.toDouble
-                         else {
-                              if (attributes(i).kind == 'r') auxVectorOutput(0) = x.toDouble
-                              else auxVectorOutput(outV.indexOf(x)) = 1 //put 1 into the active class
-                         }
-                    }
-               }
-               (auxVectorInput, auxVectorOutput)
-          }
-          this.nInstances = auxRDD.count().toInt
-          instances = auxRDD.map(x => (x._1.toVector, (x._2).toVector))
-     }
+    def loadAttributes(lines: RDD[String]) = {
 
-     def printAttributes(): Unit = {
-          attributes.foreach { x =>
-               println("Nombre: " + x.name + " Tipo: " + x.kind + " IO: " + x.IO + " Rango minimo: " + x.rangeMin + " Rango maximo: " + x.rangeMax)
-               x.valNominal.foreach(println)
-               if (x.kind == 'n' && x.IO == 'i') {
-                    println("Summary")
-                    println("InsValue")
-                    x.numInstValue.foreach(println)
-                    println("InsValueClass")
-                    x.numInsValueClass.foreach { y => y.foreach { z => print(z); print(',') }; println() }
-               }
-          }
-     }
+        var io = ' '
+        val wordsLines = lines.map(line => line.split("( *)(\\{)( *)|( *)(\\})( *)|( *)(\\[)( *)|( *)(\\])( *)|( *)(,)( *)| "))
 
-     def printInstances() = {
-          println("Numero instancias: " + instances.count())
-          instances.collect().foreach(println)
-     }
-     
-     def vectorToDouble(vec: Vector[Double]): Double ={
-          
-          var doubleValue: Double = -1
-          var array = vec.toArray
-          
-          for (i <- 0 to array.length - 1){
-               
-               if(array.apply(i)==1){
-                    doubleValue = i
-               }
-          }
-          
-          return doubleValue
-     }
+        println("Atributos: ")
+
+        attributes = wordsLines.collect().map {
+            words =>
+                words.foreach((e: String) => print(e + " "))
+                println()
+                if (outputValues.indexOf(words(1)) != -1) {
+                    io = 'o'
+                } else {
+                    io = 'i'
+                }
+                if (words(2) == "real" || words(2) == "integer") {
+                    //Constructor para atributo integer o real
+                    Attribute(words(1), 'r', io, words(3).toDouble, words(4).toDouble, Array("Void"))
+                } else {
+                    //Constructor para atributo nominal
+                    Attribute(words(1), 'n', io, 0, words.length - 3, words.slice(2, words.length))
+                }
+        }
+
+        outputValues = attributes.last.valNominal
+        attributes.last.name = outputValues(0)
+        attributes.last.rangeMax = 1
+        attributes.last.rangeMin = 0
+        attributes.last.valNominal = Array("0", "1")
+        for (i <- 1 to outputValues.length - 1) {
+            attributes = attributes :+ Attribute(outputValues(i), 'n', 'o', 0.0, 1.0, Array("0", "1"))
+        }
+
+        nOutput = outputValues.length
+        nAttributes = nInput + nOutput
+
+        attributes.foreach(atb => if (atb.kind == 'n') atb.numInstValue = Array.fill(atb.valNominal.length)(0))
+    }
+
+    def loadInstances(lines: RDD[String]) = {
+
+        println("Numero de salidas: " + nOutput)
+
+        val outV = outputValues
+
+        val auxInstances = lines.map {
+            line => line.split("( *)(,)( *)")
+        }
+        val auxRDD = auxInstances.map {
+            line =>
+                var i = -1;
+                val auxVectorInput = new Array[Double](nInput);
+                val auxVectorOutput = new Array[Double](nOutput);
+                line.foreach {
+                    x =>
+                        i = i + 1;
+                        if (attributes(i).IO == 'i') {
+
+                            if (attributes(i).kind == 'r') {
+                                auxVectorInput(i) = x.toDouble
+                            }
+                            else {
+                                auxVectorInput(i) = attributes(i).valNominal.indexOf(x)
+                                if (auxVectorInput(i) == -1) {
+                                    println("ERROR al leer linea" + x)
+                                }
+                            }
+                        } else {
+
+                            if (attributes(i).kind == 'r') {
+                                auxVectorOutput(0) = x.toDouble
+                            }
+                            else {
+                                //put 1 into the active class
+                                auxVectorOutput(outV.indexOf(x)) = 1
+                            }
+
+                        }
+                }
+                (auxVectorInput, auxVectorOutput)
+        }
+        this.nInstances = auxRDD.count().toInt
+        instances = auxRDD.map(x => (x._1.toVector, (x._2).toVector))
+    }
+
+    def printAttributes(): Unit = {
+        attributes.foreach {
+            x =>
+                println("Nombre: " + x.name + " Tipo: " + x.kind + " IO: " + x.IO + " Rango minimo: " + x.rangeMin + " Rango maximo: " + x.rangeMax)
+
+                x.valNominal.foreach((e: String) => print(e + " "))
+
+                println()
+        }
+    }
+
+    def printInstances() = {
+        println("Numero instancias: " + instances.count())
+        instances.collect().foreach(println)
+    }
+
+    def vectorToDouble(vec: Vector[Double]): Double = {
+
+        var doubleValue: Double = -1
+        val array = vec.toArray
+
+        for (i <- 0 to array.length - 1) {
+
+            if (array.apply(i) == 1) {
+                doubleValue = i
+            }
+        }
+
+        return doubleValue
+    }
 }
 
 
--- src/main/scala/prueba/Main.scala
+++ src/main/scala/prueba/Main.scala
@@ -8,21 +8,21 @@ import org.apache.spark.mllib.regression.LabeledPoint
 
 object Main {
 
-     def main(args: Array[String]) {
+    def main(args: Array[String]) {
 
-          val conf = new SparkConf().setAppName("Prueba4").setMaster("local")
-          val sc = new SparkContext(conf)
-          sc.setLogLevel("ERROR")
+        val conf = new SparkConf().setAppName("Prueba4").setMaster("local")
+        val sc = new SparkContext(conf)
+        sc.setLogLevel("ERROR")
 
-          var argumentosCorrectos = false
-          var ficheroEntrada = ""
-          var ficheroSalida = ""
-          var modelosLvl0 = Array[Array[String]]()
-          var modeloLvl1 = Array[String]()
+        var argumentosCorrectos = false
+        var ficheroEntrada = ""
+        var ficheroSalida = ""
+        var modelosLvl0 = Array[Array[String]]()
+        var modeloLvl1 = Array[String]()
 
-          if (args.length == 0) {
-               val usage =
-                    """Uso: ficheroEntrada carpetaSalida 
+        if (args.length == 0) {
+            val usage =
+                """Uso: ficheroEntrada carpetaSalida
      -l0 [modelo] [argumentos_modelo]
           modelos a utilizar en el nivel 0, se debe repetir para introducir varios modelos
      -l1 [modelo]
@@ -46,152 +46,152 @@ object Main {
      Ejemplo de uso:
           C:/iris.csv C:/resultados -l0 NB 1.0 -l0 SVM 100 -l1 LR 100 0.00000001"""
 
-               println(usage)
+            println(usage)
 
-          } else {
+        } else {
 
-               ficheroEntrada = args.apply(0)
-               ficheroSalida = args.apply(1)
+            ficheroEntrada = args.apply(0)
+            ficheroSalida = args.apply(1)
 
-               var nivel = -1
-               var contador = 0
-               var indiceModelosLvl0 = -1
+            var nivel = -1
+            var contador = 0
+            var indiceModelosLvl0 = -1
 
-               for (i <- 2 to args.length - 1) {
+            for (i <- 2 to args.length - 1) {
 
-                    contador += 1
+                contador += 1
 
-                    if (args.apply(i) == "-l0") {
-                         nivel = 0
-                         contador = 0
-                         indiceModelosLvl0 += 1
-                    }
-                    if (args.apply(i) == "-l1") {
-                         nivel = 1
-                         contador = 0
-                    }
+                if (args.apply(i) == "-l0") {
+                    nivel = 0
+                    contador = 0
+                    indiceModelosLvl0 += 1
+                }
+                if (args.apply(i) == "-l1") {
+                    nivel = 1
+                    contador = 0
+                }
 
-                    if ((nivel == 0) && (args.apply(i) != "-l0")) {
-                         if (contador == 1) {
-                              modelosLvl0 :+= Array(args.apply(i))
-                         } else {
-                              modelosLvl0.apply(indiceModelosLvl0) :+= args.apply(i)
-                         }
-
-                    } else if ((nivel == 1) && (args.apply(i) != "-l1")) {
-                         if (contador == 1) {
-                              modeloLvl1 = Array(args.apply(i))
-                         } else {
-                              modeloLvl1 :+= args.apply(i)
-                         }
-
-                    }
-               }
-
-               if (modelosLvl0.length < 2) {
-                    println("Es necesario introducir 2 o más modelos de nivel 0")
-               } else if (modeloLvl1.length == 0) {
-                    println("Es necesario introducir un modelo de nivel 1")
-               } else {
-                    println("Fichero de entrada especificado: " + ficheroEntrada)
-                    println("Carpeta de salida especificada: " + ficheroSalida)
-
-                    println("Modelos de nivel 0 especificados: ")
-                    for (i <- 0 to modelosLvl0.length - 1) {
-                         for (j <- 0 to modelosLvl0.apply(i).length - 1) {
-                              print(modelosLvl0.apply(i).apply(j) + " ")
-                         }
-                         println("")
+                if ((nivel == 0) && (args.apply(i) != "-l0")) {
+                    if (contador == 1) {
+                        modelosLvl0 :+= Array(args.apply(i))
+                    } else {
+                        modelosLvl0.apply(indiceModelosLvl0) :+= args.apply(i)
                     }
 
-                    println("Modelo de nivel 1 especificado: ")
-                    for (i <- 0 to modeloLvl1.length - 1) {
-                         print(modeloLvl1.apply(i) + " ")
+                } else if ((nivel == 1) && (args.apply(i) != "-l1")) {
+                    if (contador == 1) {
+                        modeloLvl1 = Array(args.apply(i))
+                    } else {
+                        modeloLvl1 :+= args.apply(i)
+                    }
+
+                }
+            }
+
+            if (modelosLvl0.length < 2) {
+                println("Es necesario introducir 2 o más modelos de nivel 0")
+            } else if (modeloLvl1.length == 0) {
+                println("Es necesario introducir un modelo de nivel 1")
+            } else {
+                println("Fichero de entrada especificado: " + ficheroEntrada)
+                println("Carpeta de salida especificada: " + ficheroSalida)
+
+                println("Modelos de nivel 0 especificados: ")
+                for (i <- 0 to modelosLvl0.length - 1) {
+                    for (j <- 0 to modelosLvl0.apply(i).length - 1) {
+                        print(modelosLvl0.apply(i).apply(j) + " ")
                     }
                     println("")
-                    argumentosCorrectos = true
-               }
-          }
+                }
 
-          if (argumentosCorrectos == true) {
+                println("Modelo de nivel 1 especificado: ")
+                for (i <- 0 to modeloLvl1.length - 1) {
+                    print(modeloLvl1.apply(i) + " ")
+                }
+                println("")
+                argumentosCorrectos = true
+            }
+        }
 
-               //val lines = sc.textFile(ficheroEntrada)
-               //val lines = sc.textFile("C:/Users/Pls/Desktop/iris.dat")
+        if (argumentosCorrectos == true) {
 
-               val DS = new DataSet()
-               DS.loadDataSet(ficheroEntrada, sc)
+            //val lines = sc.textFile(ficheroEntrada)
+            //val lines = sc.textFile("C:/Users/Pls/Desktop/iris.dat")
 
-               //DS.printAttributes()
-               //DS.printInstances()
+            val DS = new DataSet()
+            DS.loadDataSet(ficheroEntrada, sc)
 
-               val instancias = DS.getInstances
+            DS.printAttributes()
+            DS.printInstances()
 
-               val RDDdeLabeledPoint = instancias.map { x => LabeledPoint(DS.vectorToDouble(x._2), Vectors.dense(x._1.toArray)) }
+            val instancias = DS.getInstances
 
-               RDDdeLabeledPoint.collect().foreach(println)
+            val RDDdeLabeledPoint = instancias.map { x => LabeledPoint(DS.vectorToDouble(x._2), Vectors.dense(x._1.toArray)) }
 
-               // Dividir dataset en training y test
-               val splits = RDDdeLabeledPoint.randomSplit(Array(0.6, 0.4), seed = 11L)
-               val training = splits(0).cache()
-               val test = splits(1)
-               val numParticiones = 10
+            RDDdeLabeledPoint.collect().foreach(println)
 
-               //Paso 1
-               //Dividir el dataset en particiones
-               println("Realizando Paso 1")
-               val pesoParticiones = Array.fill(numParticiones)(1.0)
-               val arrayParticiones = training.randomSplit(pesoParticiones, seed = 11L)
+            // Dividir dataset en training y test
+            val splits = RDDdeLabeledPoint.randomSplit(Array(0.6, 0.4), seed = 11L)
+            val training = splits(0).cache()
+            val test = splits(1)
+            val numParticiones = 10
 
-               //Paso 5
-               //Repetir los pasos 2 a 4 mediante la función Stacking con varios modelos
-               var arrayResultadosNivel0 = Array[Array[RDD[LabeledPoint]]]()
-               for (i <- 0 to modelosLvl0.length - 1) {
-                    arrayResultadosNivel0 :+= StackingModelos.Stacking(arrayParticiones, test, numParticiones, modelosLvl0.apply(i))
-               }
+            //Paso 1
+            //Dividir el dataset en particiones
+            println("Realizando Paso 1")
+            val pesoParticiones = Array.fill(numParticiones)(1.0)
+            val arrayParticiones = training.randomSplit(pesoParticiones, seed = 11L)
 
-               //Paso 6
-               //Realizar un nuevo modelo con los resultados del training dataset en el paso anterior
-               println("Realizando Paso 6")
-               var combinacionTrainDatasets = arrayResultadosNivel0.apply(0).apply(0)
-               for (i <- 1 to modelosLvl0.length - 1) {
-                    combinacionTrainDatasets = combinacionTrainDatasets.union(arrayResultadosNivel0.apply(i).apply(0))
-               }
+            //Paso 5
+            //Repetir los pasos 2 a 4 mediante la función Stacking con varios modelos
+            var arrayResultadosNivel0 = Array[Array[RDD[LabeledPoint]]]()
+            for (i <- 0 to modelosLvl0.length - 1) {
+                arrayResultadosNivel0 :+= StackingModelos.Stacking(arrayParticiones, test, numParticiones, modelosLvl0.apply(i))
+            }
 
-               var combinacionTestDatasets = arrayResultadosNivel0.apply(0).apply(1)
-               for (i <- 1 to modelosLvl0.length - 1) {
-                    combinacionTestDatasets = combinacionTestDatasets.union(arrayResultadosNivel0.apply(i).apply(1))
-               }
+            //Paso 6
+            //Realizar un nuevo modelo con los resultados del training dataset en el paso anterior
+            println("Realizando Paso 6")
+            var combinacionTrainDatasets = arrayResultadosNivel0.apply(0).apply(0)
+            for (i <- 1 to modelosLvl0.length - 1) {
+                combinacionTrainDatasets = combinacionTrainDatasets.union(arrayResultadosNivel0.apply(i).apply(0))
+            }
 
-               //Paso 7
-               //Realizar predicciones con los resultados del test dataset en el paso 5
-               println("Realizando Paso 7")
-               val modeloLR = modeloLvl1.apply(0) match {
-                    //case "NB" => ModeloNaiveBayes.Modelo(combinacionTrainDatasets, modeloLvl1.apply(1).toFloat)
-                    //case "SVM" => ModeloSVM.Modelo(combinacionTrainDatasets, modeloLvl1.apply(1).toInt)
-                    case "LR" => ModeloLR.Modelo(combinacionTrainDatasets, modeloLvl1.apply(1).toInt)
-                    //case "DT"  => ModeloDT.Modelo(RDDprediccionesTraining)
-               }
+            var combinacionTestDatasets = arrayResultadosNivel0.apply(0).apply(1)
+            for (i <- 1 to modelosLvl0.length - 1) {
+                combinacionTestDatasets = combinacionTestDatasets.union(arrayResultadosNivel0.apply(i).apply(1))
+            }
 
-               val valoresCombinacionTestDatasets = combinacionTestDatasets.map({ case LabeledPoint(v1, v2) => v2 })
-               val predicciones = modeloLR.predict(valoresCombinacionTestDatasets)
-               //val prediccionesTexto = predicciones.map(f => numeroAClase(f))
-               val resultados = valoresCombinacionTestDatasets.zip(predicciones)
+            //Paso 7
+            //Realizar predicciones con los resultados del test dataset en el paso 5
+            println("Realizando Paso 7")
+            val modeloLR = modeloLvl1.apply(0) match {
+                //case "NB" => ModeloNaiveBayes.Modelo(combinacionTrainDatasets, modeloLvl1.apply(1).toFloat)
+                //case "SVM" => ModeloSVM.Modelo(combinacionTrainDatasets, modeloLvl1.apply(1).toInt)
+                case "LR" => ModeloLR.Modelo(combinacionTrainDatasets, modeloLvl1.apply(1).toInt)
+                //case "DT"  => ModeloDT.Modelo(RDDprediccionesTraining)
+            }
 
-               println("Modelo nivel 1:")
-               ModeloLR.precisionModelo(modeloLR, test)
-               //val modelo2 = ModeloLR.Modelo(training,modeloLvl1.apply(1).toInt)
-               //println("Modelo nivel 1 sin stacking:")
-               //ModeloLR.precisionModelo(modelo2, test)
+            val valoresCombinacionTestDatasets = combinacionTestDatasets.map({ case LabeledPoint(v1, v2) => v2 })
+            val predicciones = modeloLR.predict(valoresCombinacionTestDatasets)
+            //val prediccionesTexto = predicciones.map(f => numeroAClase(f))
+            val resultados = valoresCombinacionTestDatasets.zip(predicciones)
 
-               resultados.saveAsTextFile(ficheroSalida)
-               println("Ended successfully")
+            println("Modelo nivel 1:")
+            ModeloLR.precisionModelo(modeloLR, test)
+            //val modelo2 = ModeloLR.Modelo(training,modeloLvl1.apply(1).toInt)
+            //println("Modelo nivel 1 sin stacking:")
+            //ModeloLR.precisionModelo(modelo2, test)
 
-          }
-     }
+            resultados.saveAsTextFile(ficheroSalida)
+            println("Ended successfully")
 
-     def printInstances(instances: RDD[String]) = {
-          println("Numero instancias: " + instances.count())
-          instances.collect().foreach(println)
-     }
+        }
+    }
+
+    def printInstances(instances: RDD[String]) = {
+        println("Numero instancias: " + instances.count())
+        instances.collect().foreach(println)
+    }
 
 }
